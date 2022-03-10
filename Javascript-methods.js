e59f620c3192247e2b44c0fd41bf190c09144015
TahminaM <Tahminamunni07@gmail.com>
2022-03-10 18:45:01 -0500

    Added myPush method

--- Javascript-methods.js
+++ Javascript-methods.js
@@ -35,11 +35,22 @@ Array.prototype.myEach = function (callbackFn) {
   Array.prototype.myIndexOf = function() {
     // Place your code here.
   };
+ 
+  
   
 // PUSH //
-  Array.prototype.myPush = function() {
-    // Place your code here.
-  };
+Array.prototype.myPush = function(...args) {  // Use rest parameter to accept any number of input arguments
+    let args_index = 0;  // Index of new element
+    let length = this.length;   // Length of "this" array
+    // The last element of "this" array is at length - 1
+    for (let i = length; i < length + args.length; i++) {
+        this[i] = args[args_index];  // Add new element to end of "this" array
+        args_index++;
+    }
+    return this.length;  // Return new length of "this" array
+};
+
+
   
 // LASTINDEXOF //
   Array.prototype.myLastIndexOf = function() {
