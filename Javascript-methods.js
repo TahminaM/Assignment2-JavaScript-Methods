bf0e614b09bf5f81224c6c2373cc4b4f3e8a9c06
Ramon Torres <RTorres5318@gmail.com>
2022-03-12 16:35:37 -0500

    Created Section for my code implementations (RT)

--- Javascript-methods.js
+++ Javascript-methods.js
@@ -10,7 +10,6 @@ Array.prototype.myEach = function (callbackFn) {
     }
 };
 
-
 // MAP //
   Array.prototype.myMap = function() {
     // Place your code here.
@@ -66,3 +65,73 @@ Array.prototype.myPush = function(...args) {  // Use rest parameter to accept an
   Object.myValues = function() {
     // Place your code here.
   };
+
+
+// *********** Begin Ramon Torres Implementations ***********
+
+// FOR EACH //
+Array.prototype.myEach = function (cbFunc) {
+    for (let i = 0; i < this.length; i++) {
+        if (this[i] === undefined) continue;
+
+        cbFunc(this[i], i, this);
+    }
+};
+
+// MAP //
+Array.prototype.myMap = function () {
+    // Place your code here.
+};
+
+// SOME //
+Array.prototype.mySome = function () {
+    // Place your code here.
+};
+
+// REDUCE //
+Array.prototype.myReduce = function () {
+    // Place your code here.
+};
+
+// INCLUDES //
+Array.prototype.myIncludes = function () {
+    // Place your code here.
+};
+
+// INDEXOF //
+Array.prototype.myIndexOf = function () {
+    // Place your code here.
+};
+
+
+
+// PUSH //
+Array.prototype.myPush = function (...elemsToAdd) {  
+    let elems_index = 0;  
+    let length = this.length;   
+    
+    for (let i = length; i < length + elemsToAdd.length; i++) {
+        this[i] = elemsToAdd[elems_index];  // Add new element to end of "this" array
+        elems_index++;
+    }
+    return this.length;  // Return new length of "this" array
+};
+
+
+
+// LASTINDEXOF //
+Array.prototype.myLastIndexOf = function () {
+    // Place your code here.
+};
+
+// KEYS //
+Object.myKeys = function () {
+    // Place your code here.
+};
+
+// VALUES //
+Object.myValues = function () {
+    // Place your code here.
+};
+
+// *********** End Ramon Torres Implementations ***********
