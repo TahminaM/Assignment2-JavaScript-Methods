7bec652029dc7685fa10e24421649e9a6d3bb62f
Ramon Torres <RTorres5318@gmail.com>
2022-03-13 18:49:33 -0400

    Implemented myReduce function

--- Javascript-methods.js
+++ Javascript-methods.js
@@ -20,11 +20,6 @@ Array.prototype.myEach = function (callbackFn) {
     // Place your code here.
   };
   
-// REDUCE //
-  Array.prototype.myReduce = function() {
-    // Place your code here.
-  };
-  
 // INCLUDES //
   Array.prototype.myIncludes = function() {
     // Place your code here.
@@ -34,8 +29,6 @@ Array.prototype.myEach = function (callbackFn) {
   Array.prototype.myIndexOf = function() {
     // Place your code here.
   };
- 
-  
   
 // PUSH //
 Array.prototype.myPush = function(...args) {  // Use rest parameter to accept any number of input arguments
@@ -48,8 +41,6 @@ Array.prototype.myPush = function(...args) {  // Use rest parameter to accept an
     }
     return this.length;  // Return new length of "this" array
 };
-
-
   
 // LASTINDEXOF //
   Array.prototype.myLastIndexOf = function() {
@@ -69,15 +60,6 @@ Array.prototype.myPush = function(...args) {  // Use rest parameter to accept an
 
 // *********** Begin Ramon Torres Implementations ***********
 
-// FOR EACH //
-Array.prototype.myEach = function (cbFunc) {
-    for (let i = 0; i < this.length; i++) {
-        if (this[i] === undefined) continue;
-
-        cbFunc(this[i], i, this);
-    }
-};
-
 // MAP //
 Array.prototype.myMap = function () {
     // Place your code here.
@@ -90,25 +72,24 @@ Array.prototype.mySome = function () {
 
 // REDUCE //
 Array.prototype.myReduce = function (cbFunc, initialVal) {
-    if (this.length === 0 && initialVal) return initialVal;
-    if (this.length === 1 && initialVal) return cbFunc(initialVal, this[0], 0, this);
-    else if (this.length === 1 && !initialVal) return this[0];
+    if (this.length === 0 && !initialVal) {
+        throw new TypeError();
+    }
 
-    let prev = this[0];
+    let acc = (initialVal !== undefined) ? initialVal : undefined;
 
-    if (!initialVal) {
-        for (let i = 0; i < this.length; i++) {
-            if (this[i] === undefined) continue;
+    for (let i = 0; i < this.length; i++) {
+        if (this[i] === undefined) continue;
 
-            cbFunc(prev, i, this);
-        }
-    } else {
-        for (let i = 0; i < this.length; i++) {
-            if (this[i] === undefined) continue;
-
-            cbFunc(this[i], i, this);
+        if (acc !== undefined) {
+            acc = cbFunc(acc, this[i], i, this);
+        } else {
+            acc = this[i]
         }
     }
+
+    return acc;
+
 };
 
 // INCLUDES //
@@ -119,18 +100,6 @@ Array.prototype.myIncludes = function () {
 // INDEXOF //
 Array.prototype.myIndexOf = function () {
     // Place your code here.
-};
-
-// PUSH //
-Array.prototype.myPush = function (...elemsToAdd) {  
-    let elems_index = 0;  
-    let length = this.length;   
-    
-    for (let i = length; i < length + elemsToAdd.length; i++) {
-        this[i] = elemsToAdd[elems_index];  // Add new element to end of "this" array
-        elems_index++;
-    }
-    return this.length;  // Return new length of "this" array
 };
 
 // LASTINDEXOF //
